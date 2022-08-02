3afbdfbf33a327a25af346306baeb8e1f2a715f7
skyclouds2001 <skyclouds2001@163.com>
2022-08-03 01:06:23 +0800

    init project

---
+++ miniprogram/app.ts
@@ -0,0 +1,11 @@
+App<IAppOption>({
+  globalData: {},
+  onLaunch() {
+    wx.login({
+      success: res => {
+        console.log(res.code)
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      },
+    });
+  },
+});
