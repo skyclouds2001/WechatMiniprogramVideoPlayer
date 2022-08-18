b34b863fee9aa1cc4a6752f4c4bfb3a3e8b60b11
skyclouds2001 <skyclouds2001@163.com>
2022-08-18 12:37:54 +0800

    feat: add basic img

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,11 +1,7 @@
 App<IAppOption>({
+
   globalData: {},
-  onLaunch() {
-    wx.login({
-      success: res => {
-        console.log(res.code)
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      },
-    });
-  },
+
+  onLaunch() {},
+
 });
--- miniprogram/conponents/video-player/video-player.ts
+++ miniprogram/conponents/video-player/video-player.ts
@@ -1,15 +1,38 @@
-Component({
+interface VideoPlayerData extends WechatMiniprogram.Component.DataOption {
+  video: WechatMiniprogram.VideoContext | null
+}
+interface VideoPlayerProperty extends WechatMiniprogram.Component.PropertyOption {
+  src: WechatMiniprogram.Component.FullProperty<StringConstructor>,
+  title: WechatMiniprogram.Component.FullProperty<StringConstructor>,
+}
+interface VideoPlayerMethod extends WechatMiniprogram.Component.MethodOption {}
+
+Component<VideoPlayerData, VideoPlayerProperty, VideoPlayerMethod>({
 
   properties: {
-
+    src: {
+      type: String,
+      value: 'https://v.jiemian.com/d4/95/d4952f9be1b4731488339b7efb524010_3072.mp4',
+    },
+    title: {
+      type: String,
+      value: ''
+    }
   },
 
   data: {
-
+    video: null,
   },
 
-  methods: {
+  methods: {},
 
+  lifetimes: {
+    created: function () {},
+    attached: function () {
+      this.createSelectorQuery().select('.video-player-context').context(node => this.setData({
+        video: node as unknown as WechatMiniprogram.VideoContext,
+      })).exec();
+    },
   },
 
 });
